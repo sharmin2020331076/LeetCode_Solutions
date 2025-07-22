9a5a31841fa07b797a1b7971e3e56d1db808c4b9
Sharmin Akther Juthi <107158456+sharmin2020331076@users.noreply.github.com>
2025-07-22 12:37:25 +0600

    Time: 4 ms (33.25%) | Memory: 8.4 MB (99.63%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -4,15 +4,14 @@ public:
         if (x < 0) return false;
 
         long long rem = 0;
-        int original = x;
-
+        int ori = x;
         while (x != 0) {
             int digit = x % 10;
             rem = rem * 10 + digit;
             x /= 10; 
         }
 
-        if (rem == original && rem <= INT_MAX && rem >= INT_MIN)
+        if (rem == ori && rem <= INT_MAX && rem >= INT_MIN)
             return true;
         else
             return false;
