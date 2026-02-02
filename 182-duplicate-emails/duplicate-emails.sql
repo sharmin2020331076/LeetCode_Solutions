1d994ff2ad33c64effdc10b1d4949e65db742af6
Sharmin Akther <107158456+sharmin2020331076@users.noreply.github.com>
2026-02-02 23:27:00 +0600

    Time: 325 ms (94.67%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
+/*
 select distinct p.email as Email 
 from Person p
 join Person p1 
-on p.email = p1.email and p.id != p1.id
+on p.email = p1.email and p.id != p1.id*/
+
+select email as Email 
+from Person 
+group by email 
+having count(email) > 1
