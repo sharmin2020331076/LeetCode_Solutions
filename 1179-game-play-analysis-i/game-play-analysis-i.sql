2583e842e276f7f3533135b9bf141a4690229e41
Sharmin Akther <107158456+sharmin2020331076@users.noreply.github.com>
2026-02-03 20:12:49 +0600

    Time: 455 ms (77.84%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,5 +1,23 @@
 # Write your MySQL query statement below
+/*
 select player_id,MIN(event_date) as first_login
 from Activity 
 group by player_id
+
+select distinct a.player_id,
+(
+    select MIN(event_date) 
+    from Activity
+    where player_id = a.player_id
+
+) as first_login
+from Activity a
+*/
+
+select distinct player_id,
+MIN(event_date) over (partition by player_id) as first_login
+from Activity
+
+
+
 
