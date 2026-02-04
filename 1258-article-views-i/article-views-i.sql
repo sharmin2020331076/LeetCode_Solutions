66fca616a8111e679bef94d2ef790a4acf18405d
Sharmin Akther <107158456+sharmin2020331076@users.noreply.github.com>
2026-02-04 18:11:59 +0600

    Time: 609 ms (6.05%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,9 +1,11 @@
 # Write your MySQL query statement below
 select author_id as id 
-from Views 
+from Views
+
 where author_id = viewer_id
 group by author_id 
 having count(*) >= 1
+
 order by author_id 
 
 
