2067a5fc5585384b4e1e9962dfa8385045e577ac
Sharmin Akther <107158456+sharmin2020331076@users.noreply.github.com>
2026-02-08 23:24:56 +0600

    Time: 3 ms (74.01%) | Memory: 10 MB (93.31%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,52 +1,62 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int n = s.length();
-        int rom = 0;
-        for(int i=0;i<n;i++){
+        int ans = 0;
+        for(int i=0; i<s.size(); i++){
             if(s[i]=='M')
-            rom +=1000;
-
-            else if(i!=n-1 && s[i]=='C' && s[i+1]=='M'){
-                rom+=900;
-                i++;
-            }
-            else if(i!=n-1 && s[i]=='C' && s[i+1]=='D'){
-                rom+=400;
+            ans+=1000;
+            else if(i!=s.size()-1 && s[i]=='C' && s[i+1]=='M')
+            {
+                ans+=900;
                 i++;
             }
 
-            else if(i!=n-1 && s[i]=='X' && s[i+1]=='C'){
-                rom+=90;
-                i++;
-            }
-            else if(i!=n-1 && s[i]=='X' && s[i+1]=='L'){
-                rom+=40;
-                i++;
-            }
-            else if(i!=n-1 && s[i]=='I' && s[i+1]=='X'){
-                rom+=9;
-                i++;
-            }
-            else if(i!=n-1 && s[i]=='I' && s[i+1]=='V'){
-                rom+=4;
+           else if(s[i] == 'D')
+           ans+=500;
+           else if(i!=s.size()-1 && s[i]=='C' && s[i+1]=='D')
+            {
+                ans+=400;
                 i++;
             }
 
-            else if(s[i]=='I')
-            rom +=1;
-            else if(s[i]=='V')
-            rom +=5;
-            else if(s[i]=='X')
-            rom +=10;
-            else if(s[i]=='L')
-            rom +=50;
-            else if(s[i]=='C')
-            rom +=100;
-            else if(s[i]=='D')
-            rom +=500;
+           else if(s[i] == 'C')
+           ans+=100;
+           else if(i!=s.size()-1 && s[i]=='X' && s[i+1]=='C')
+            {
+                ans+=90;
+                i++;
+            }
+
+           else if(s[i] == 'L')
+           ans+=50;
+           else if(i!=s.size()-1 && s[i]=='X' && s[i+1]=='L')
+            {
+                ans+=40;
+                i++;
+            }
+
+           else if(s[i] == 'X')
+           ans+=10;
+           else if(i!=s.size()-1 && s[i]=='I' && s[i+1]=='X')
+            {
+                ans+=9;
+                i++;
+            }
+            else if(i!=s.size()-1 && s[i]=='I' && s[i+1]=='V')
+            {
+                ans+=4;
+                i++;
+            }
+
+           else if(s[i] == 'V')
+           ans+=5;
+
+           else if(s[i]=='I')
+           ans+=1;
+           
+
+           
         }
-
-        return rom;
+        return ans;
     }
 };
