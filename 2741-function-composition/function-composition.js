08e249a20857cc1dc739ffda8f4432ecadb216de
Sharmin Akther <107158456+sharmin2020331076@users.noreply.github.com>
2026-02-09 22:21:19 +0600

    Time: 65 ms (12.30%) | Memory: 55.5 MB (29.02%) - LeetSync

---
+++ 2741-function-composition/function-composition.js
@@ -0,0 +1,10 @@
+const compose = function(functions) {
+    return function(x) {
+        let val = x;
+        for(let i = functions.length - 1; i >= 0; i--){
+            val = functions[i](val);
+        }
+        return val;
+    };
+    
+};
