54fd803fcccc2c94e081cdb12e9fe6805074ee17
Sharmin Akther <107158456+sharmin2020331076@users.noreply.github.com>
2026-02-09 19:37:58 +0600

    Time: 38 ms (85.24%) | Memory: 55 MB (6.96%) - LeetSync

---
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -0,0 +1,8 @@
+const filter = (arr, fn) =>{
+    const filteredArr = [];
+    for(let i=0; i<arr.length; i++){
+        if(fn(arr[i],i))
+        filteredArr.push(arr[i]);
+    }
+    return filteredArr;
+}
